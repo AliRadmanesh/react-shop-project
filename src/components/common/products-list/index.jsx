aa1269a4f58a5cd7df6776cc000c0c2000b4b16f
AliRadmanesh <ali.radmanesh1995@gmail.com>
2020-12-14 01:05:33 +0330

    Add ProductsList component

---
+++ src/components/common/products-list/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProductsList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Products...</p>
+      </div>
+    );
+  }
+}
+
+export default ProductsList;
