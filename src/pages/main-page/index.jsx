43c95b3780ff4f3dd85adf3874ce9445dbdcfd1a
AliRadmanesh <ali.radmanesh1995@gmail.com>
2020-12-15 00:13:22 +0330

    Add Cart component to main page

--- src/pages/main-page/index.jsx
+++ src/pages/main-page/index.jsx
@@ -4,6 +4,7 @@ import Header from "../../components/layout/header";
 import NavBar from "../../components/common/nav-bar";
 import Footer from "../../components/layout/footer";
 import ProductsList from "../../components/common/products-list";
+import Cart from "../../components/common/cart";
 
 class MainPage extends Component {
   render() {
@@ -12,6 +13,7 @@ class MainPage extends Component {
         <Header>
           <NavBar />
         </Header>
+        <Cart />
         <ProductsList />
         <Footer />
       </div>
