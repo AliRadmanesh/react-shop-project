920469df8c1d09abaa0575a06eed6aaae32fed55
AliRadmanesh <ali.radmanesh1995@gmail.com>
2020-12-16 21:50:41 +0330

    Add loading component

---
+++ src/components/base/loading/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import "./style.css";
+
+class Loading extends Component {
+  render() {
+    return (
+      <div className="loadingio-spinner-ripple-qhdcuck6pf">
+        <div className="ldio-xqsfwq4vc8">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Loading;
