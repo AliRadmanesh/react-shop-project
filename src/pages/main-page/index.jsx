942cfb5670e0903b7d5810c34425ca7baf99191a
AliRadmanesh <ali.radmanesh1995@gmail.com>
2020-12-16 16:05:34 +0330

    Add central state for cart items & implement add and remove to cart feature

--- src/pages/main-page/index.jsx
+++ src/pages/main-page/index.jsx
@@ -7,14 +7,45 @@ import ProductsList from "../../components/common/products-list";
 import Cart from "../../components/common/cart";
 
 class MainPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      addedToCartList: [],
+    };
+  }
+
+  handleOnProductSelected = (productItem) => {
+    const { addedToCartList } = this.state;
+
+    if (!addedToCartList.includes(productItem)) {
+      this.setState((prevState) => {
+        return {
+          addedToCartList: [...prevState.addedToCartList, productItem],
+        };
+      });
+    }
+  };
+
+  handleRemoveProduct = (productItem) => {
+    const { addedToCartList } = this.state;
+
+    const filteredCartItems = addedToCartList.filter((cartItem) => {
+      return cartItem.id !== productItem.id;
+    });
+    this.setState({ addedToCartList: filteredCartItems });
+  };
+
   render() {
+    const { addedToCartList } = this.state;
+
     return (
       <div className="main-layout">
         <Header>
           <NavBar />
         </Header>
-        <Cart />
-        <ProductsList />
+        <Cart list={addedToCartList} onItemRemove={this.handleRemoveProduct} />
+        <ProductsList onProductSelected={this.handleOnProductSelected} />
         <Footer />
       </div>
     );
