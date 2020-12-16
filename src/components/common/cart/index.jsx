5476ac6b61d96f8a6025bb707f8bdcced217a95c
AliRadmanesh <ali.radmanesh1995@gmail.com>
2020-12-16 22:47:34 +0330

    Add show/hide & remove item feature to cart "

--- src/components/common/cart/index.jsx
+++ src/components/common/cart/index.jsx
@@ -3,19 +3,41 @@ import React, { Component } from "react";
 import "./style.css";
 
 class Cart extends Component {
-  render() {
-    const { list, onItemRemove } = this.props;
+  renderCart = () => {
+    const { isCartVisible, list, onItemRemove } = this.props;
 
-    return list.map((productItem) => {
+    if (isCartVisible) {
+      if (!list.length) {
+        return (
+          <div className="cart-empty-container">
+            <p>There is no product in your cart :(</p>
+          </div>
+        );
+      }
+
       return (
-        <div className="product-in-cart" key={productItem.id}>
-          <p>{productItem.name}</p>
-          <p>{productItem.price}</p>
-          {productItem.hasOff && <p>Sale</p>}
-          <button onClick={() => onItemRemove(productItem)}>Delete</button>
+        <div className="cart-container">
+          {list.map((productItem) => {
+            return (
+              <div className="product-in-cart" key={productItem.id}>
+                <p>{productItem.name}</p>
+                <p>{productItem.price}</p>
+                {productItem.hasOff && <p>Sale</p>}
+                <button onClick={() => onItemRemove(productItem)}>
+                  Delete
+                </button>
+              </div>
+            );
+          })}
         </div>
       );
-    });
+    }
+
+    return null;
+  };
+
+  render() {
+    return <>{this.renderCart()}</>;
   }
 }
 
