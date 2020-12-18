729db69edc467e1aeea035fd8c08c023cd1ddfbb
AliRadmanesh <ali.radmanesh1995@gmail.com>
2020-12-18 23:39:42 +0330

    Add NotFound route component

---
+++ src/components/common/not-found/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+import { paths } from "../../../routes/paths";
+
+class NotFound extends Component {
+  render() {
+    return <Redirect to={paths.ERROR_404} />;
+  }
+}
+
+export default NotFound;
