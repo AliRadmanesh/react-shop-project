a0e84a93ef8afeb250bfe59a13d36e759e8e7eaa
AliRadmanesh <ali.radmanesh1995@gmail.com>
2020-12-18 23:38:51 +0330

    Remove useless codes

--- src/components/layout/header/index.jsx
+++ src/components/layout/header/index.jsx
@@ -3,10 +3,6 @@ import React, { Component } from "react";
 import styles from "./style.module.css";
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const { children } = this.props;
 
