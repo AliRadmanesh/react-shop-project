6d723c74487fd603774dca1610afcb4ac3080eec
AliRadmanesh <ali.radmanesh1995@gmail.com>
2020-12-18 23:40:41 +0330

    Complete route structure

---
+++ src/routes/index.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
+import { routes } from "./routes";
+import AuthRoute from "./auth-route";
+
+class RouteHandler extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: true,
+    };
+  }
+
+  render() {
+    const { isAuthenticated } = this.state;
+
+    return (
+      <div>
+        <Switch>
+          {routes.map((routeItem) => {
+            const { name, path, exact, needAuth, component } = routeItem;
+
+            if (needAuth) {
+              return (
+                <AuthRoute
+                  key={name}
+                  path={path}
+                  exact={exact}
+                  component={component}
+                  isAuthenticated={isAuthenticated}
+                />
+              );
+            }
+
+            return (
+              <Route
+                key={name}
+                path={path}
+                exact={exact}
+                component={component}
+              />
+            );
+          })}
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default RouteHandler;
