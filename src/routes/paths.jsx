d31f86cef3a1047efb23b48ee488fa122c7325ae
AliRadmanesh <ali.radmanesh1995@gmail.com>
2020-12-18 23:36:58 +0330

    Add route paths

---
+++ src/routes/paths.jsx
@@ -0,0 +1,10 @@
+export const paths = {
+  HOME: "/",
+  CART: "/cart",
+  PROFILE: "/profile",
+  PRODUCTS_LIST: "/products-list",
+  PRODUCT_ITEM: "/product-item/:productId",
+  NOT_AUTHENTICATED: "/restricted",
+  ERROR_404: "/404",
+  NOT_FOUND: "*",
+};
