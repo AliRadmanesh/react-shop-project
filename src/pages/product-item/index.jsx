555d4f3b9b644bb3011854f9955e990f2355b3a5
AliRadmanesh <ali.radmanesh1995@gmail.com>
2021-01-10 23:27:59 +0330

    Show each project info on its own page

--- src/pages/product-item/index.jsx
+++ src/pages/product-item/index.jsx
@@ -1,11 +1,52 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
-class ProductItemPage extends Component {
-  render() {
-    const { productId } = this.props.match.params;
+import { getProductById } from "server";
+import Loading from "components/base/loading";
 
-    return <div>Product Id #{productId}</div>;
+class ProductItemPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { productId } = this.props.match.params;
+    this.getProductData(productId);
+  }
+
+  getProductData = async (productId) => {
+    try {
+      const data = await getProductById(productId);
+      this.setState({ data });
+    } catch (e) {
+      alert("Product NOT Found :(");
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  renderProduct = () => {
+    const { isLoading, data } = this.state;
+
+    if (isLoading) {
+      return <Loading />;
+    }
+    return (
+      <>
+        <p>Id: {data.id}</p>
+        <p>Name: {data.name}</p>
+        <p>Price: {data.price}</p>
+        {data.hasOff && <p>Sale!</p>}
+      </>
+    );
+  };
+
+  render() {
+    return <div className="product-wrapper">{this.renderProduct()}</div>;
   }
 }
 
